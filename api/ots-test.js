2e8cbdc3c18b13cb69e09b395d6c5b545b8438fb
Sami-Signium <sami.hamid@signium.com>
2026-04-13 10:02:36 +0200

    Update ots-test.js

--- api/ots-test.js
+++ api/ots-test.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   
   try {
     const OTS_KEY = process.env.OTS_KEY;
-    const url = `https://www.ots.at/api/?AppID=${OTS_KEY}&Suchbegriff=Vorstand&Anzahl=5&Zeitraum=48&outputFormat=json`;
+    const url = `https://www.ots.at/api/liste?app=${OTS_KEY}&query=Vorstand+CEO+Geschäftsführer&inhalt=alle&anz=10&sourcetype=OTS&format=json`;
     
     const r = await fetch(url);
     const text = await r.text();
